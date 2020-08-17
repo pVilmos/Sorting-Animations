416727a09101cb97ec16846f075ab7f546063102
pVilmos <65255166+pVilmos@users.noreply.github.com>
2020-08-17 16:06:12 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,50 +1,64 @@
-const types = 2;
+const types = 3;
 for (var i = 0; i < types; i++) {
-  var create_button = document.createElement("button");
-  document.body.appendChild(create_button);
-  document.body.lastChild.setAttribute("id", "button" + i);
+var create_button = document.createElement("button");
+document.body.appendChild(create_button);
+document.body.lastChild.setAttribute("id", "button" + i);
 }
 document.getElementById('button' + 0).innerHTML = "Bubble Sort";
 document.getElementById('button' + 1).innerHTML = "Selection Sort";
+document.getElementById('button' + 2).innerHTML = "Insertion Sort";
+
 
 setTimeout(function(){
-  for (var i = 0; i < types; i++) {
-    document.getElementById("button" + i).style.color = "black";
-    document.getElementById("button" + i).style.backgroundColor = "#dddddd";
-  }
+for (var i = 0; i < types; i++) {
+  document.getElementById("button" + i).style.color = "black";
+  document.getElementById("button" + i).style.backgroundColor = "#dddddd";
+}
 }, 500)
 
 
 //Getiing functions when clicking
 document.getElementById("button0").addEventListener("click", bubble_sort_algorithm);
 document.getElementById("button0").addEventListener("click", function() {
-  for (var i = 0; i < types; i++) {
-    document.getElementById("button" + i).style.color = "#fff";
-    document.getElementById("button" + i).style.backgroundColor = "#fff";
-  }
+for (var i = 0; i < types; i++) {
+  document.getElementById("button" + i).style.color = "#fff";
+  document.getElementById("button" + i).style.backgroundColor = "#fff";
+}
 });
 document.getElementById("button0").addEventListener("click", function(){
-  setTimeout(function(){
-    for (var i = 0; i < types; i++) {
-      document.getElementById("button" + i).remove();
-    }
-  }, 500)
+setTimeout(function(){
+  for (var i = 0; i < types; i++) {
+    document.getElementById("button" + i).remove();
+  }
+}, 500)
 });
 
 document.getElementById("button1").addEventListener("click", selection_sort_algorithm);
 document.getElementById("button1").addEventListener("click", function() {
-  for (var i = 0; i < types; i++) {
-    document.getElementById("button" + i).style.color = "#fff";
-    document.getElementById("button" + i).style.backgroundColor = "#fff";
-    setTimeout(function() {
-      document.getElementById("button" + i).remove();
-    }, 1000)
-  }
+for (var i = 0; i < types; i++) {
+  document.getElementById("button" + i).style.color = "#fff";
+  document.getElementById("button" + i).style.backgroundColor = "#fff";
+}
 });
-document.getElementById("button0").addEventListener("click", function(){
-  setTimeout(function(){
-    for (var i = 0; i < types; i++) {
-      document.getElementById("button" + i).remove();
-    }
-  }, 500)
+document.getElementById("button1").addEventListener("click", function(){
+setTimeout(function(){
+  for (var i = 0; i < types; i++) {
+    document.getElementById("button" + i).remove();
+  }
+}, 500)
+});
+
+document.getElementById("button2").addEventListener("click", insertion_sort_algorithm);
+document.getElementById("button2").addEventListener("click", function() {
+for (var i = 0; i < types; i++) {
+  document.getElementById("button" + i).style.color = "#fff";
+  document.getElementById("button" + i).style.backgroundColor = "#fff";
+}
+});
+document.getElementById("button2").addEventListener("click", function(){
+setTimeout(function(){
+  for (var i = 0; i < types; i++) {
+    document.getElementById("button" + i).remove();
+  }
+}, 500)
 });
